6dce563f3ba10c40b64eb4ef4ed12b58d563401e
thuyenvie <anthuyen2k5@gmail.com>
2026-08-16 19:25:26 +0700

    feat(chapter02): final

--- Project/smart-campus/app/(tabs)/index.tsx
+++ Project/smart-campus/app/(tabs)/index.tsx
@@ -1,5 +1,9 @@
 import { StyleSheet, View } from 'react-native';
-import CampusDashboard from '../../components/CampusDashboard/CampusDashboard';
+// import CampusDashboard from '../../components/CampusDashboard/CampusDashboard';
+// import CampusDashboard from '../../components/CampusDashboard/CampusDashboard-SectionList';
+// import CampusDashboard from '../../components/CampusDashboard/CampusDashboard-ResponsiveCard';
+// import CampusDashboard from '../../components/CampusDashboard/CampusDashboard-Keyboard';
+import CampusDashboard from '../../components/CampusDashboard/CampusDashboard-Validation';
 
 export default function HomeScreen() {
   return (
